07e2841972a08db32ded6cd1f0c03436c2d74558
Luca <coduri.vitor@gmail.com>
2024-01-14 16:42:44 +0100

    fuzzy finder with refs

--- rust/src/domain/mod.rs
+++ rust/src/domain/mod.rs
@@ -1,95 +1,47 @@
-use std::fs;
+#![allow(dead_code)]
+
+mod linux;
+
+use std::borrow::{Borrow, Cow};
 
 use anyhow::Result;
-use freedesktop_desktop_entry::{default_paths, DesktopEntry};
 use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
 
-trait Application {
+pub trait Application {
     fn name(&self) -> &str;
     fn exec(&self) -> Result<()>;
 }
 
-#[derive(Debug)]
-struct LinuxApplication {
-    name: String,
-    exec: String,
-    icon: Option<String>,
-    keywords: Option<String>,
+pub enum EntityType {
+    App,
+    Command,
 }
 
-impl LinuxApplication {
-    fn from_desktop_entry(entry: &DesktopEntry<'_>) -> Option<Self> {
-        let name = entry.name(None)?.to_string();
-        let exec = entry.exec()?.to_string();
-        let icon = entry.icon().map(|i| i.to_string());
-        let keywords = entry.keywords().map(|s| s.to_string());
-
-        Some(Self {
-            name,
-            exec,
-            icon,
-            keywords,
-        })
-    }
-
-    fn get_applications() -> Vec<Self> {
-        let mut result = Vec::new();
-        for path in default_paths() {
-            if let Ok(files) = fs::read_dir(&path) {
-                for file in files {
-                    let path_file = file.unwrap().path();
-
-                    if let Ok(bytes) = fs::read_to_string(&path_file) {
-                        let entry = DesktopEntry::decode(&path, &bytes).unwrap();
-                        let app = LinuxApplication::from_desktop_entry(&entry);
-                        if let Some(app) = app {
-                            result.push(app);
-                        }
-                    }
-                }
-            }
-        }
-        result
-    }
+pub struct Entity<'a> {
+    name: Cow<'a, str>,
+    description: Cow<'a, str>,
+    alias: Cow<'a, str>,
+    command: Box<dyn Fn() -> Result<()> + 'a>,
+    r#type: EntityType,
 }
 
-impl Application for LinuxApplication {
-    fn name(&self) -> &str {
-        &self.name
-    }
-
-    fn exec(&self) -> Result<()> {
-        todo!()
-    }
+pub struct FuzzyFinder<'a> {
+    entities: Vec<Entity<'a>>,
 }
 
-enum Entity {
-    // TODO better not use box for perf
-    App(Box<dyn Application>),
-    Clipboard,
-    Link,
-}
-
-struct FuzzyFinder {
-    entities: Vec<Entity>,
-}
-
-impl FuzzyFinder {
-    pub fn search(&self, search: &str) -> Vec<(i64, &Entity)> {
+impl<'a> FuzzyFinder<'a> {
+    pub fn search(&self, search: &str) -> Vec<(i64, &Entity<'a>)> {
         let matcher = SkimMatcherV2::default();
 
         let mut result: Vec<(i64, &Entity)> = Vec::new();
-        for (index, entity) in self.entities.iter().enumerate() {
-            let score = match entity {
-                Entity::App(app) => matcher.fuzzy_match(app.name(), search),
-                Entity::Clipboard => None,
-                Entity::Link => None,
-            };
+        for entity in self.entities.iter() {
+            let score = matcher.fuzzy_match(entity.name.borrow(), search);
+
             if let Some(score) = score {
                 let pos = result
                     .binary_search_by(|(curr_score, _)| score.cmp(curr_score))
                     .unwrap_or_else(|e| e);
-                result.insert(pos, (score, &self.entities[index]))
+                result.insert(pos, (score, entity))
             }
         }
         result
@@ -98,31 +50,52 @@ impl FuzzyFinder {
 
 #[cfg(test)]
 mod test {
-    use super::{Entity, FuzzyFinder, LinuxApplication};
+
+    use crate::domain::{linux::LinuxApplication, Application, EntityType};
+
+    use super::{Entity, FuzzyFinder};
 
     #[test]
-    fn get_applications() {
+    fn run_app() {
         let apps = LinuxApplication::get_applications();
-        println!("{:?}", apps.len());
+        let mut entities: Vec<_> = Vec::new();
+        for app in apps.iter() {
+            let entity = Entity {
+                name: app.name().into(),
+                description: "test".into(),
+                alias: "alias".into(),
+                command: Box::new(move || app.exec()),
+                r#type: EntityType::App,
+            };
+            entities.push(entity);
+        }
+
+        let fuzzy_finder = FuzzyFinder { entities };
+        let results = fuzzy_finder.search("firefox");
+        let (_score, entity) = results[0];
+        (*entity.command)().expect("Couldn't run the program succesfully");
     }
 
     #[test]
     fn search_app() {
         let apps = LinuxApplication::get_applications();
-        let entities: Vec<_> = apps
-            .into_iter()
-            .map(|app| Entity::App(Box::new(app)))
-            .collect();
+        let mut entities: Vec<_> = Vec::new();
+        for app in apps.iter() {
+            let entity = Entity {
+                name: app.name().into(),
+                description: "test".into(),
+                alias: "alias".into(),
+                command: Box::new(move || app.exec()),
+                r#type: EntityType::App,
+            };
+            entities.push(entity);
+        }
 
         let fuzzy_finder = FuzzyFinder { entities };
         let results = fuzzy_finder.search("fir");
         println!("{:?}", results.len());
-        for result in results {
-            match result.1 {
-                Entity::App(app) => println!("Score: {}, Entity: {}", result.0, app.name()),
-                Entity::Clipboard => todo!(),
-                Entity::Link => todo!(),
-            }
+        for (score, entity) in results {
+            println!("Score: {}, Entity: {}", score, entity.name);
         }
     }
 }

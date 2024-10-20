1a1b6f5d843e11eca147147f1bcababd4899922b
Luca <coduri.vitor@gmail.com>
2024-10-20 23:59:08 +0200

    feature: Tauri V2.0

--- generate.sh
+++
@@ -1,2 +0,0 @@
-export CPATH="$(clang -v 2>&1 | grep "Selected GCC installation" | rev | cut -d' ' -f1 | rev)/include"
-flutter_rust_bridge_codegen generate
--- lib/plugin_screen_service.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-class PluginScreenService extends ChangeNotifier {}
